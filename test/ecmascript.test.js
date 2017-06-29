cb00de139c5d858bd347b06881029e9598346106
Jason Lenoble <jason.lenoble@gmail.com>
2017-06-29 13:51:19 +0200

    Passing 'pass' tests up to 7

--- test/ecmascript.test.js
+++ test/ecmascript.test.js
@@ -6,7 +6,7 @@ describe('Testing ECMAScript', function () {
 
   const muter = Muter(process.stderr, 'write'); // eslint-disable-line
 
-  for (let i = 0; i < 5/*1956*/; i++) {
+  for (let i = 0; i < 8/*1956*/; i++) {
     let file;
 
     if (
