b44ebc29c3b982e38199b47f1f9b02a6bbbe2ed0
Jason Lenoble <jason.lenoble@gmail.com>
2017-07-09 21:35:21 +0200

    Now clean task also cleans parserDir

--- gulp/clean.js
+++ gulp/clean.js
@@ -2,7 +2,7 @@ import gulp from 'gulp';
 import del from 'del';
 
 export const clean = () => {
-  return del('build');
+  return del(['build', 'src/static/antlr4/parsers']);
 };
 
 gulp.task('clean', clean);
